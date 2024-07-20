fa39394f99fb0118f556a474e92d086f0ada9d0c
ewertondrigues02 <ewertonrodrigues01@gmail.com>
2024-07-20 07:52:45 -0300

    Projeto criado - Gestão de Pessoas

---
+++ src/main/java/br/com/ewerton/gestaopessoa/GestaoPessoaApplication.java
@@ -0,0 +1,13 @@
+package br.com.ewerton.gestaopessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoPessoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoPessoaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/ewerton/gestaopessoa/GestaoPessoaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ewerton.gestaopessoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoPessoaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
